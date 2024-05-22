d2104a30c7a70e4766a1f94dc0452f297fcdd40d
Phl0h_CC <phloh@gmx.de>
2024-05-22 10:48:33 +0200

    new class Cat

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,6 +4,17 @@ public class App {
     
     public static void main(String[] args) {
 
+        //zwei Objekte erzeugen
+
+        // Cat --> Referenztyp (Klasse aus dem das Objekt gebildet wird)
+        // cat1 --> Referenzvariable (hält die Adresse zum Objekt (im RAM bzw. Heap))
+        Cat cat1 = new Cat(); // Instanziierung >> Objekt
+        System.out.println(cat1); // Adresse des Objekts
+ 
+        Cat cat2 = new Cat();
+        System.out.println(cat2);
+        
+
 
     }
 
