682b2c51a832458e175c79f38a6cbf283c327f9f
Phloh_local <phloh1338@gmx.de>
2024-05-22 15:46:28 +0200

    refactoring output seminar

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,22 +4,21 @@ public class App {
     
     public static void main(String[] args) {
 
-        //zwei Objekte erzeugen
-
         // Cat --> Referenztyp (Klasse aus dem das Objekt gebildet wird)
         // cat1 --> Referenzvariable (hält die Adresse zum Objekt (im RAM bzw. Heap))
+
         Cat cat1 = new Cat(); // Instanziierung >> Objekt
-        System.out.println("Blick von aussen: " + cat1); // Adresse des Objekts
-        cat1.sayHi();
-        System.out.println("---------------------");
+        output("Blick von aussen: " + cat1); // Adresse des Objekts
+        output("Blick von innen: " + cat1.getInstanceVar());
+
+        output("---------------------");
+
         Cat cat2 = new Cat();
-        System.out.println("Blick von aussen: " + cat2);
-        cat2.sayHi();
+        output("Blick von aussen: " + cat2);
+        output("Blick von innen: " + cat2.getInstanceVar());
         
-        output("hi");
-
     }
-
+    // statische Methode die in der Klasse ausgeführt wird
     public static void output(String outputStr){
         System.out.println(outputStr);
    }
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -3,10 +3,19 @@ package com.cc.java;
 public class Cat {
 
     // this --> Instanzvariable
-    public void sayHi(){
-        System.out.println("Blick von innen: " + this);
+    // public void sayHi(){
+    //     System.out.println("Blick von innen: " + this);
         
-    }
+    // }
 
+//     public void sayHi(){
+//         App.output("Blick von innen: " + this);
+        
+
+//     }
+
+    public Cat getInstanceVar(){
+        return this;
+    }
 
 }
