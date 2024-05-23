3672788e71a450a42ed1a7c4f9558db190df69ff
Phl0h_CC <phloh@gmx.de>
2024-05-23 10:44:07 +0200

    Wertzuweisung zwier Objekte[D[D[D[D[D[D[D[D[D[D[D[3~[F

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -11,12 +11,36 @@ public class App {
         output("Blick von aussen: " + cat1); // Adresse des Objekts
         output("Blick von innen: " + cat1.getInstanceVar());
 
-        output("---------------------");
+        //Werte zuweisen
+        cat1.name = "Grizabella";
+        cat1.furColor = "White";
+        cat1.age = 29;
+
+        //Werte ausgeben
+        // output(); // --> Fehler und nicht undefined wie bei JS, da ein bereits definierter Typ erwartet wird (untypisiert <-> typisiert)
+        // output(null); // null --> leere Objektreferenz (geht als einziges abgesehen vom akzeptierten Argument(hier wird String erwartet))
+        output(cat1.name);
+        output(cat1.furColor);
+        output(Integer.toString(cat1.age)); // Typkonvertierung // output(String.valueOf(cat1.age)); // kommt zum selben Ergebnis
+        
+        // output("---------------------");
 
         Cat cat2 = new Cat();
         output("Blick von aussen: " + cat2);
         output("Blick von innen: " + cat2.getInstanceVar());
-        
+
+        //Werte zuweisen
+        cat2.name = "Alonzo";
+        cat2.furColor = "Grey";
+        cat2.age = 35;
+
+        //Werte ausgeben
+        // output(); // --> Fehler und nicht undefined wie bei JS, da ein bereits definierter Typ erwartet wird (untypisiert <-> typisiert)
+        // output(null); // null --> leere Objektreferenz (geht als einziges abgesehen vom akzeptierten Argument(hier wird String erwartet))
+        output(cat2.name);
+        output(cat2.furColor);
+        output(Integer.toString(cat2.age)); // Typkonvertierung // output(String.valueOf(cat1.age)); // kommt zum selben Ergebnis
+
     }
     // statische Methode die in der Klasse ausgeführt wird
     public static void output(String outputStr){
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,20 +2,21 @@ package com.cc.java;
 
 public class Cat {
 
-    // this --> Instanzvariable
-    // public void sayHi(){
-    //     System.out.println("Blick von innen: " + this);
-        
-    // }
+    public String name;
+    public String furColor;
+    public int age;
 
-//     public void sayHi(){
-//         App.output("Blick von innen: " + this);
-        
-
-//     }
-
+    
+    // Adresse der Objektinstanz vom Typ Cat
     public Cat getInstanceVar(){
         return this;
     }
 
+    // this --> Instanzvariable
+    // public void sayHi(){
+    //     System.out.println("Blick von innen: " + this);
+    // }
+    //public void sayHi(){
+    //App.output("Blick von innen: " + this);
+    // }
 }
